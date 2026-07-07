a402aa1fe9e21bf92c4776f07f8814ff08106cc4
ulimax42064 <ulimax420640@gmail.com>
2026-07-07 06:02:03 +0000

    uihuigsd

--- TUPMundial.Web/obj/Debug/net10.0/TUPMundial.Web.AssemblyInfo.cs
+++ TUPMundial.Web/obj/Debug/net10.0/TUPMundial.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TUPMundial.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ba51d2cf96d0d9cb208a3cde7f75fff0055589b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aad3a907b78cc071e32eac2b3e3e7ff0cddb82f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TUPMundial.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TUPMundial.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
